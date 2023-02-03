c3d4614bd9a54d28278ab43fbb120166d8038141
Artur_Bahdanau <zenn19952013@gmail.com>
2023-02-03 12:23:02 +0300

    added changes: conversions to all types

--- untitled/src/by/itacademy/courses/homework_2/task4.java
+++ untitled/src/by/itacademy/courses/homework_2/task4.java
@@ -9,14 +9,14 @@ public class task4 {
         float numberFloat = 80.7000005f;
         double numberDouble = 60.465454654559;
         char meaningchar = 0x1D;
-        transformationByte(nuberByte, numberShort, numberInt, numberLong, numberDouble, numberFloat, meaningchar);
-        transformationShort(numberShort, numberInt, numberLong, numberDouble, numberFloat);
-        transformationInt(numberInt, numberLong, numberDouble, numberFloat);
-        transformationLong(numberLong, numberDouble, numberFloat);
-        transformationFloat(numberDouble, numberFloat);
-        transformationDouble(numberInt, numberLong, numberDouble, numberFloat);
-        transformationChar(nuberByte, numberShort, numberInt, numberLong, numberDouble, numberFloat, meaningchar);
 
+        transformationByte(nuberByte, numberShort, numberInt, numberLong, numberDouble, numberFloat, meaningchar);
+        transformationShort(nuberByte, numberShort, numberInt, numberLong, numberDouble, numberFloat, meaningchar);
+        transformationInt(nuberByte, numberShort, numberInt, numberLong, numberDouble, numberFloat, meaningchar);
+        transformationLong(nuberByte, numberShort, numberInt, numberLong, numberDouble, numberFloat, meaningchar);
+        transformationFloat(nuberByte, numberShort, numberInt, numberLong, numberDouble, numberFloat, meaningchar);
+        transformationDouble(nuberByte, numberShort, numberInt, numberLong, numberDouble, numberFloat, meaningchar);
+        transformationChar(nuberByte, numberShort, numberInt, numberLong, numberDouble, numberFloat, meaningchar);
     }
 
     public static void transformationByte(byte numberByte, short numberShort, int numberInt, long numberLong,
@@ -28,53 +28,73 @@ public class task4 {
         numberFloat = (float) numberByte;
         numberDouble = (double) numberByte;
         meanigchar = (char) numberByte;
-        System.out.println("Transformation byte in: " + "\nByte: " + numberByte + "\n" + "Short: " + numberShort + "\n"
-                + "Int: " + numberInt + "\n" + "Long: " + numberLong + "\n" + "Float: " + numberFloat + "\n"
-                + "Double: " + numberDouble + "\n" + "Char: " + meanigchar + "\n");
+        System.out.println("Transformation byte in: " + "\nByte: " + numberByte + "\nShort: " + numberShort
+                + "\nInt: " + numberInt + "\nLong: " + numberLong + "\nFloat: " + numberFloat + "\nDouble: " + numberDouble + "\nChar: " + meanigchar + "\n");
     }
 
-    public static void transformationShort(short numberShort, int numberInt, long numberLong, double numberDouble,
-                                           float numberFloat) {
+    public static void transformationShort(byte numberByte, short numberShort, int numberInt, long numberLong,
+                                           double numberDouble, float numberFloat, char meanigchar) {
         numberShort = numberShort;
+        numberByte = (byte) numberShort;
         numberInt = (int) numberShort;
         numberLong = (long) numberShort;
         numberFloat = (float) numberShort;
         numberDouble = (double) numberShort;
-        System.out.println("Transformation short in: " + "\n" + "Short: " + numberShort + "\n" + "Int: " + numberInt + "\n"
-                + "Long: " + numberLong + "\n" + "Float: " + numberFloat + "\n" + "Double: " + numberDouble + "\n");
+        meanigchar = (char) numberByte;
+        System.out.println("Transformation short in: " + "\nShort: " + numberShort + "\nByte: " + numberByte + "\nInt: " + numberInt +
+                "\nLong: " + numberLong + "\nFloat: " + numberFloat + "\nDouble: " + numberDouble + "\nChar: " + meanigchar + "\n");
     }
 
-    public static void transformationInt(int numberInt, long numberLong, double numberDouble, float numberFloat) {
+    public static void transformationInt(byte numberByte, short numberShort, int numberInt, long numberLong,
+                                         double numberDouble, float numberFloat, char meanigchar) {
         numberInt = numberInt;
+        numberByte = (byte) numberInt;
+        numberShort = (short) numberInt;
         numberLong = (long) numberInt;
         numberFloat = (float) numberInt;
         numberDouble = (double) numberInt;
-        System.out.println("Transformation integer in: " + "\n" + "Int: " + numberInt + "\n" + "Long: " + numberLong
-                + "\n" + "Float: " + numberFloat + "\n" + "Double: " + numberDouble + "\n");
+        meanigchar = (char) numberInt;
+        System.out.println("Transformation integer in: " + "\nInt: " + numberInt + "\nByte: " + numberByte + "\nShort: " + numberShort +
+                "\nLong: " + numberLong + "\nFloat: " + numberFloat + "\nDouble: " + numberDouble + "\nChar: " + meanigchar + "\n");
     }
 
-    public static void transformationLong(long numberLong, double numberDouble, float numberFloat) {
+    public static void transformationLong(byte numberByte, short numberShort, int numberInt, long numberLong,
+                                          double numberDouble, float numberFloat, char meanigchar) {
         numberLong = numberLong;
+        numberByte = (byte) numberLong;
+        numberShort = (short) numberLong;
+        numberInt = (int) numberLong;
         numberFloat = (float) numberLong;
         numberDouble = (double) numberLong;
-        System.out.println("Transformation long in: " + "\n" + "Long: " + numberLong + "\n" + "Float: "
-                + numberFloat + "\n" + "Double: " + numberDouble + "\n");
+        meanigchar = (char) numberLong;
+        System.out.println("Transformation long in: " + "\nLong: " + numberLong + "\nByte: " + numberByte + "\nShort: " + numberShort + "\nInt: " + numberInt
+                + "\nFloat: " + numberFloat + "\nDouble: " + numberDouble + "\nChar: " + meanigchar + "\n");
     }
 
-    public static void transformationFloat(double numberDouble, float numberFloat) {
+    public static void transformationFloat(byte numberByte, short numberShort, int numberInt, long numberLong,
+                                           double numberDouble, float numberFloat, char meanigchar) {
         numberFloat = numberFloat;
+        numberByte = (byte) numberFloat;
+        numberShort = (short) numberFloat;
+        numberInt = (int) numberFloat;
+        numberLong = (long) numberFloat;
         numberDouble = (double) numberFloat;
-        System.out.println("Transformation float in: " + "\n" + "Float: " + numberFloat + "\n" + "Double: "
-                + numberDouble + "\n");
+        meanigchar = (char) numberFloat;
+        System.out.println("Transformation float in: " + "\nFloat: " + numberFloat + "\nByte: " + numberByte + "\nShort: " + numberShort
+                + "\nInt: " + numberInt + "\nLong: " + numberLong + "\nDouble: " + numberDouble + "\nChar: " + meanigchar + "\n");
     }
 
-    public static void transformationDouble(int numberInt, long numberLong, double numberDouble, float numberFloat) {
+    public static void transformationDouble(byte numberByte, short numberShort, int numberInt, long numberLong,
+                                            double numberDouble, float numberFloat, char meanigchar) {
         numberDouble = numberDouble;
-        numberLong = (long) numberDouble;
-        numberInt = (int) numberDouble;
+        numberByte = (byte) numberFloat;
+        numberShort = (short) numberFloat;
+        numberInt = (int) numberFloat;
+        numberLong = (long) numberFloat;
         numberFloat = (float) numberDouble;
-        System.out.println("Transformation double in: " + "\n" + "Float: " + numberFloat + "\n" + "Double: "
-                + numberDouble + "\n");
+        meanigchar = (char) numberFloat;
+        System.out.println("Transformation double in: " + "\nDouble: " + numberDouble + "\nByte: " + numberByte + "\nShort: " + numberShort
+                + "\nInt: " + numberInt + "\nLong: " + numberLong + "\nFloat: " + numberFloat + "\n");
     }
 
     public static void transformationChar(byte numberByte, short numberShort, int numberInt, long numberLong,
@@ -87,8 +107,7 @@ public class task4 {
         numberFloat = (float) meanigchar;
         numberDouble = (double) meanigchar;
 
-        System.out.println("Transformation char in: " + "\n" + "Char: " + meanigchar + "\nByte: " + numberByte + "\n" +
-                "Short: " + numberShort + "\n" + "Int: " + numberInt + "\n" + "Long: " + numberLong + "\n"
-                + "Float: " + numberFloat + "\n" + "Double: " + numberDouble + "\n");
+        System.out.println("Transformation char in: " + "\nChar: " + meanigchar + "\nByte: " + numberByte + "\nShort: " + numberShort + "\nInt: "
+                + numberInt + "\nLong: " + numberLong + "\nFloat: " + numberFloat + "\nDouble: " + numberDouble + "\nChar: " + meanigchar + "\n");
     }
 }
