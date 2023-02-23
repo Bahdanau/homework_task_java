00b3800c5c1203ad3417804335805b747606ee91
Artur_Bahdanau <zenn19952013@gmail.com>
2023-02-23 08:53:50 +0300

    change file Task5

--- src/by/itacademy/courses/hw4/Task5.java
+++ src/by/itacademy/courses/hw4/Task5.java
@@ -5,6 +5,7 @@ public class Task5 {
         method1();
         method2();
     }
+
     public static void method1() {
         int[] myArray = new int[]{97, 45, 7, 100, 54, 67, 54, 406, 52, 2};
         int indexOfMin = 0;
@@ -22,10 +23,8 @@ public class Task5 {
         int[] myArray = new int[]{97, 45, 7, 100, 54, 67, 54, 406, 52, 2};
         int indexOfMin = 0;
         for (int i = 1; i < myArray.length; i = i + 2) {
-            if (i % 2 != 0) {
-                if (myArray[i] < myArray[indexOfMin]) {
-                    indexOfMin = i;
-                }
+            if (myArray[i] < myArray[indexOfMin]) {
+                indexOfMin = i;
             }
         }
         System.out.println("Индекс минимального значения в массиве методом 2: " + indexOfMin + " Его значение: " + myArray[indexOfMin]);
